999e841e0bad75c0f08defd672548307db85a2b4
Mayankbhardwaj255 <mayankbhardwaj708@gmail.com>
2025-07-16 22:38:00 +0530

    Add two numbers fix

--- 2 Add Two Numbers/2 Add Two Numbers.java
+++ 2 Add Two Numbers/2 Add Two Numbers.java
@@ -1,10 +1,3 @@
-Most asked problems at FAANG companies: https://docs.google.com/spreadsheets/d/1hzP8j7matoUiJ15N-RhsL5Dmig8_E3aP/edit#gid=1377915986
-
-Leetcode Link: https://leetcode.com/problems/add-two-numbers/
-
-Video Solution: https://www.youtube.com/watch?v=MYtp__JpTns
-
-**************** Java Solution ***********************
 
 
 /**

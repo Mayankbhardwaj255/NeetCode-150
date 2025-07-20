e5d0d4b34908556659f06fa9e4767fed6f9514c4
Mayankbhardwaj255 <mayankbhardwaj708@gmail.com>
2025-07-21 02:42:19 +0530

    search a 2d matrix

---
+++ 74 Search a 2D Matrix/74 Search a 2D Matrix.java
@@ -0,0 +1,30 @@
+
+// Leetcode Link:  https://leetcode.com/problems/search-a-2d-matrix/
+
+**************** Java Solution ***********************
+// treat this 2d matrix as a one d array and apply binary search technique.
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int left = 0;
+        int right = m * n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / n][mid % n];
+            
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+}

51c6a2fbf86904ae72e47cfd2d863e61c02f2d97
Mayankbhardwaj255 <mayankbhardwaj708@gmail.com>
2025-07-21 01:29:47 +0530

    Daily temperatures

---
+++ 739 Daily Temperatures/739 Daily Temperatures.java
@@ -0,0 +1,43 @@
+// Leetcode Link:  https://leetcode.com/problems/daily-temperatures/
+// Brute Forece: gives TLE:-
+
+    class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (temperatures[j] > temperatures[i]) {
+                    res[i] = j - i;
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+
+// Optimal Approach:-
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}

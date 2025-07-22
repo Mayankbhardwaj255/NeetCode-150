dbb46ef5abb3cb5532adad4ac43ad9973d0703ec
Mayankbhardwaj255 <mayankbhardwaj708@gmail.com>
2025-07-22 23:10:08 +0530

    reverse integer

---
+++ 7 Reverse Integer/7 Reverse Integer.java
@@ -0,0 +1,28 @@
+// Leetcode Link: https://leetcode.com/problems/reverse-integer/
+
+
+**************** Java Solution ***********************
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        
+        while (x != 0) {
+            // Extract the last digit
+            int pop = x % 10;
+            x /= 10;
+            
+            // Check for overflow before actually updating the reversed number
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;  // Overflow case
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;  // Underflow case
+            }
+            
+            // Update the reversed number
+            rev = rev * 10 + pop;
+        }
+        
+        return rev;
+    }
+}
